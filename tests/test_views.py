0a8cce20ec3d335bd66c0c950e08ea9055724c89
Christian Hartung <hartung@live.com>
2024-09-02 19:11:36 -0300

    chore: add py.typed marker

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,36 +1,41 @@
 from http import HTTPStatus
+from typing import TYPE_CHECKING
 
 import pytest
+from django.test import AsyncClient, Client
 from django.urls import reverse
+
+if TYPE_CHECKING:
+    from django.http import HttpResponse
 
 pytestmark = pytest.mark.django_db
 
 
 class TestLivenessView:
-    def test_get_ping(self, client):
-        response = client.get(reverse("django_healthy:ping"))
+    def test_get_ping(self, client: Client):
+        response: HttpResponse = client.get(reverse("django_healthy:ping"))
 
         assert response.status_code == HTTPStatus.OK
 
     @pytest.mark.parametrize("method", ["post", "put", "patch", "delete"])
-    def test_methods_not_allowed(self, method, client):
+    def test_methods_not_allowed(self, method: str, client: Client):
         django_client_method = getattr(client, method)
 
-        response = django_client_method(reverse("django_healthy:ping"))
+        response: HttpResponse = django_client_method(reverse("django_healthy:ping"))
 
         assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
 
     @pytest.mark.asyncio
-    async def test_get_ping_async(self, async_client):
-        response = await async_client.get(reverse("django_healthy:ping"))
+    async def test_get_ping_async(self, async_client: AsyncClient):
+        response: HttpResponse = await async_client.get(reverse("django_healthy:ping"))
 
         assert response.status_code == HTTPStatus.OK
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize("method", ["post", "put", "patch", "delete"])
-    async def test_methods_not_allowed_async(self, method, async_client):
+    async def test_methods_not_allowed_async(self, method: str, async_client: AsyncClient):
         django_client_method = getattr(async_client, method)
 
-        response = await django_client_method(reverse("django_healthy:ping"))
+        response: HttpResponse = await django_client_method(reverse("django_healthy:ping"))
 
         assert response.status_code == HTTPStatus.METHOD_NOT_ALLOWED
