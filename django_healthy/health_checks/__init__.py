d7bf5d47659cf53ce33eb0afe260f7fd58592141
Christian Hartung <hartung@live.com>
2024-10-02 23:03:51 -0300

    feat: add health view

--- django_healthy/health_checks/__init__.py
+++ django_healthy/health_checks/__init__.py
@@ -1,7 +1,13 @@
 from .base import HealthCheck, HealthCheckResult, HealthStatus
+from .service import HealthCheckService, HealthReport, HealthReportEntry
 
 __all__ = [
     "HealthCheck",
     "HealthCheckResult",
     "HealthStatus",
+    "HealthCheckService",
+    "HealthReport",
+    "HealthReportEntry",
 ]
+
+health_check_service = HealthCheckService()
